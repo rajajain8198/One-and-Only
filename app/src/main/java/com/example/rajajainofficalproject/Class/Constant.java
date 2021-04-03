648024a57dff9711766af4b667140c8be90032d1
rajajain <raja.jain@mindruby.com>
2021-04-03 11:42:22 +0530

    Commit for adding Constant File

---
+++ app/src/main/java/com/example/rajajainofficalproject/Class/Constant.java
@@ -0,0 +1,10 @@
+package com.example.rajajainofficalproject.Class;
+
+public class Constant {
+
+    public static final String Firebase_Storage_Path_For_Image = "User_Data/";
+    public static final String Firebase_Database_Reference_Path = "User_Details";
+
+    public static final String Shared_Preferences = "user_details";
+    public static final String Shared_Preferences_User_Unique_ID = "user_unique_id";
+}
